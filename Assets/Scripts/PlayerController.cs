d94ce96933813316285836dba600a158b9bcde4b
sanixto <sanix.to@gmail.com>
2023-10-27 19:47:17 +0300

    add life logic

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,15 +6,22 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float speed = 5f;
+    [SerializeField] private int lives = 5;
     [SerializeField] private float jumpForce = 4.6f; 
 
+    private Vector2 startPosition = new Vector2(-15, 0);
     private bool isGrounded;
+    private readonly float deathDelay = 0.3f;
     private Rigidbody2D rb; 
     private CapsuleCollider2D feetCollider;
 
     private void Awake() {
         rb = GetComponent<Rigidbody2D>();
         feetCollider = GetComponent<CapsuleCollider2D>();
+    }
+
+    private void Start() {
+        Debug.Log("Your lives: " + lives);
     }
 
     void FixedUpdate () {
@@ -31,6 +38,17 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    private void OnTriggerEnter2D (Collider2D other) {
+        if (other.tag == "Water") {
+            GetDamage();
+            if (lives == 0) {
+                Die();
+                return;
+            }
+            transform.position = startPosition;
+        }
+    }
+
     private void Run() {
         Vector3 curPosition = transform.position;
         Vector3 dir = transform.right * Input.GetAxis("Horizontal");
@@ -44,5 +62,15 @@ public class PlayerController : MonoBehaviour
     private void CheckGround() {
         LayerMask groundMask = LayerMask.GetMask("Ground");
         isGrounded = feetCollider.IsTouchingLayers(groundMask);
+    }
+
+    private void GetDamage() {
+        lives -= 1;
+        Debug.Log("Your lives: " + lives);
+    }
+
+    private void Die() {
+        Destroy(rb.GameObject(), deathDelay);
+        Debug.Log("Game over");
     }
 }
