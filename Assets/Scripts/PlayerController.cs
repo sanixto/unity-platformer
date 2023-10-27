a8dc490c33daa6f4a3eb46e91db3959461ab23db
sanixto <sanix.to@gmail.com>
2023-10-27 16:43:24 +0300

    add jump for Hero

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,11 +6,19 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float speed = 5f;
+    [SerializeField] private float jumpForce = 4.6f; 
 
+    private bool isGrounded;
     private Rigidbody2D rb; 
+    private CapsuleCollider2D feetCollider;
 
     private void Awake() {
         rb = GetComponent<Rigidbody2D>();
+        feetCollider = GetComponent<CapsuleCollider2D>();
+    }
+
+    void FixedUpdate () {
+        CheckGround();
     }
 
     void Update()
@@ -18,11 +26,23 @@ public class PlayerController : MonoBehaviour
         if (Input.GetButton("Horizontal")) {
             Run();
         }
+        if (isGrounded && Input.GetButton("Jump")) {
+            Jump();
+        }
     }
 
     private void Run() {
         Vector3 curPosition = transform.position;
         Vector3 dir = transform.right * Input.GetAxis("Horizontal");
         transform.position = Vector3.MoveTowards(curPosition, curPosition + dir, speed * Time.deltaTime);
+    }
+
+    private void Jump() {
+        rb.velocity += new Vector2(0f, jumpForce);
+    }
+
+    private void CheckGround() {
+        LayerMask groundMask = LayerMask.GetMask("Ground");
+        isGrounded = feetCollider.IsTouchingLayers(groundMask);
     }
 }
